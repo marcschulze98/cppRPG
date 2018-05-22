5de052f1daaa4c6a1b6f263b93ea1a3673850432
Marc Schulze <marc.aurel.schulze98@gmail.com>
2018-05-22 19:30:27 +0200

    made baseclass constexpr

--- include/cpprpg/classes/skills.hpp
+++ include/cpprpg/classes/skills.hpp
@@ -3,18 +3,41 @@
 
 #include <cstdio>
 
+enum class SkillType {SELF, TARGETED, AREA};
+
+struct skill_arg
+{
+
+};
+// to be able to put skills in arrays they need to be the same type,
+// so just make different constructors of Skill for different skills
 class Skill
 {
-	Skill() = delete;
-};
-
-class FistAttack: public Skill
-{
+	private:
 	public:
-		constexpr static char skillname[] = "Punch";
-		FistAttack()
+		constexpr Skill(const char* name, const char* description, void (*use_skill)(skill_arg arg), SkillType type):
+		name(name), description(description), use_skill(use_skill), type(type) {}
+
+		const char* name;
+		const char* description;
+		int level = 0;
+		void (*use_skill)(skill_arg arg);
+		SkillType type;
+
+
+		constexpr static Skill FistAttack()
 		{
-			puts("PUNCH!");
+			return Skill("Punch", "Inflicts basic attack damage to an enemy", nullptr, SkillType::TARGETED);
+		}
+
+		constexpr static Skill Bash()
+		{
+			return Skill("Bash", "Inflicts 2 * basic attack damage to an enemy", nullptr, SkillType::TARGETED);
+		}
+
+		void operator()(skill_arg arg)
+		{
+			use_skill(arg);
 		}
 };
 
--- src/mainController.cpp
+++ src/mainController.cpp
@@ -3,4 +3,13 @@
 int main()
 {
 	BaseClass test;
+	puts(test.skill_list[1].name);
+
+	// doesn't compile:
+	// test.skill_list[0].level++ <- tries to increment ro-object
+	// solution: copy object
+
+	Skill copied = test.skill_list[0];
+	copied.level++;
+	printf("%d\n",copied.level);
 }
